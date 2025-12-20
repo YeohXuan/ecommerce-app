fad65e6b6f11fd4656e30c60a6783a3b6318e626
Yeoh Xuan <yorkxuan7@gmail.com>
2025-12-21 00:22:08 +0800

    Implement navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,134 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { Link, NavLink } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import { useState } from "react";
 
-export default Navbar
+const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} className="w-36" alt="logo" />
+
+      {/* Center of navigation bar */}
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1 uppercase">
+          <p>Home</p>
+          <hr className="hidden w-1/2 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink
+          to="/collection"
+          className="flex flex-col items-center gap-1 uppercase"
+        >
+          <p>Collection</p>
+          <hr className="hidden w-1/2 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink
+          to="/about"
+          className="flex flex-col items-center gap-1 uppercase"
+        >
+          <p>About</p>
+          <hr className="hidden w-1/2 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className="flex flex-col items-center gap-1 uppercase"
+        >
+          <p>Contact</p>
+          <hr className="hidden w-1/2 border-none h-[1.5px] bg-gray-700" />
+        </NavLink>
+      </ul>
+
+      {/* Right side of navigation bar */}
+      <div className="flex items-center gap-6">
+        <img
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt="search-icon"
+        />
+        <div className="group relative">
+          <img
+            src={assets.profile_icon}
+            className="w-5 cursor-pointer"
+            alt="profile-icon"
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-4 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img
+            src={assets.cart_icon}
+            className="w-5 cursor-pointer"
+            alt="cart-icon"
+          />
+          <p className="absolute -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square text-[8px] rounded-full">
+            0
+          </p>
+        </Link>
+
+        {/* Mobile menu icon */}
+        <img
+          src={assets.menu_icon}
+          alt="menu-icon"
+          className="w-5 cursor-pointer sm:hidden"
+          onClick={() => setIsVisible(!isVisible)}
+        />
+      </div>
+
+      {/* Sidebar menu for mobile */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          isVisible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            className="flex items-center gap-4 p-3 cursor-pointer"
+            onClick={() => setIsVisible(false)}
+          >
+            <img
+              src={assets.dropdown_icon}
+              alt="dropdown-icon"
+              className="h-4 rotate-180"
+            />
+            <p>Back</p>
+          </div>
+          <NavLink
+            to="/"
+            className="py-2 pl-6 border"
+            onClick={() => setIsVisible(false)}
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            to="/collection"
+            className="py-2 pl-6 border"
+            onClick={() => setIsVisible(false)}
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="py-2 pl-6 border"
+            onClick={() => setIsVisible(false)}
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className="py-2 pl-6 border"
+            onClick={() => setIsVisible(false)}
+          >
+            CONTACT
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

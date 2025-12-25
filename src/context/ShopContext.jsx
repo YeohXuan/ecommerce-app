612afef43354e996806420ba52c1b9c7bf1609ed
Yeoh Xuan <yorkxuan7@gmail.com>
2025-12-25 19:40:58 +0800

    Implement ShopContext

---
+++ src/context/ShopContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+import { products } from "../../assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = ({ children }) => {
+  const currency = "RM";
+  const delivery_fee = 5;
+
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+  };
+
+  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
+};
+
+export default ShopContextProvider;

0da7eb32868e1b44f6ec90d0e6ea10a99d7fc040
Yeoh Xuan <yorkxuan7@gmail.com>
2026-01-07 17:15:11 +0800

    Added searchbar context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link, NavLink } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import { useState } from "react";
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [isVisible, setIsVisible] = useState(false);
+  const { setShowSearch } = useContext(ShopContext);
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -47,6 +50,7 @@ const Navbar = () => {
           src={assets.search_icon}
           className="w-5 cursor-pointer"
           alt="search-icon"
+          onClick={() => setShowSearch((prev) => !prev)}
         />
         <div className="group relative">
           <img

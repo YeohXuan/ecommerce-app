a9156472c9176fa6a4509b0a8288c14d6c85efa4
Yeoh Xuan <yorkxuan7@gmail.com>
2026-01-31 14:03:25 +0800

    Refactor background logic

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -13,71 +13,109 @@ const Product = () => {
   const { currency } = useContext(ShopContext);
 
   const fetchProductData = async () => {
-    products.map((item) => {
-      if (item._id === productId) {
-        setProductData(item);
-        setImage(item.image[0]);
-        console.log(productData);
-      }
-    });
+    const product = products.find((item) => item._id === productId);
+    if (product) {
+      setProductData(product);
+      setImage(product.image[0]);
+    }
   };
   useEffect(() => {
     fetchProductData();
-  });
+    console.log("Products in context:", products);
+  }, [productId, products]);
 
   return productData ? (
-    <div className="flex gap-12 flex-col sm:flex-row pt-10 border-t border-gray-300">
-      <div className="flex flex-col sm:flex-row-reverse gap-3">
-        <img src={image} alt={productData.name} className="w-full h-auto" />
-        <img
-          src={productData.image[0]}
-          alt={productData.name}
-          className="w-[24%]"
-        />
-      </div>
-      <div className="">
-        <p className="font-medium text-2xl mt-2">{productData.name}</p>
-        <div className="flex gap-1 mt-2 items-center">
-          <img src={assets.star_icon} className="w-3" />
-          <img src={assets.star_icon} className="w-3" />
-          <img src={assets.star_icon} className="w-3" />
-          <img src={assets.star_icon} className="w-3" />
-          <img src={assets.star_dull_icon} className="w-3.5" />
-          <p className="pl-2">(122)</p>
-        </div>
-        <p className="font-medium text-3xl mt-5">
-          {currency}
-          {productData.price}
-        </p>
-        <p className="my-5 font-light">{productData.description}</p>
-        <div className="flex flex-col gap-4 my-8">
-          <p>Select Size</p>
-          <div className="flex gap-2">
-            <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
-              S
-            </button>
-            <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
-              M
-            </button>
-            <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
-              L
-            </button>
-            <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
-              XL
-            </button>
-            <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
-              XXL
-            </button>
+    <div className="border-t-2 border-gray-200 pt-10 transition-opacity ease-in duration-50 opacity-100">
+      <div className="flex gap-12 flex-col sm:flex-row">
+        <div className="flex flex-1 flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                src={item}
+                key={index}
+                onClick={() => setImage(item)}
+                alt={productData.name}
+                className="w-[24%] sm:w-full sm:mb-3 shrink-0 cursor-pointer"
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} alt={productData.name} className="w-full h-auto" />
           </div>
         </div>
-        <button className="bg-gray-900 text-zinc-100 py-3 px-6 text-sm cursor-pointer">
-          ADD TO CART
-        </button>
-        <hr className="border-gray-300 mt-8 mb-5" />
-        <div className="flex flex-col gap-1 font-light text-sm">
-          <p>100% Original product.</p>
-          <p>Cash on delivery is available on this product.</p>
-          <p>Easy return and exchange policy within 7 days.</p>
+
+        {/* Product details */}
+        <div className="flex-1">
+          <p className="font-medium text-2xl mt-2">{productData.name}</p>
+          <div className="flex gap-1 mt-2 items-center">
+            <img src={assets.star_icon} className="w-3" />
+            <img src={assets.star_icon} className="w-3" />
+            <img src={assets.star_icon} className="w-3" />
+            <img src={assets.star_icon} className="w-3" />
+            <img src={assets.star_dull_icon} className="w-3.5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="font-medium text-3xl mt-5">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="my-5 font-light md:w-4/5">{productData.description}</p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
+                S
+              </button>
+              <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
+                M
+              </button>
+              <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
+                L
+              </button>
+              <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
+                XL
+              </button>
+              <button className="px-4 py-2 bg-gray-100 border border-gray-200 cursor-pointer">
+                XXL
+              </button>
+            </div>
+          </div>
+          <button className="bg-gray-900 text-zinc-100 py-3 px-6 text-sm cursor-pointer">
+            ADD TO CART
+          </button>
+          <hr className="border-gray-300 mt-8 mb-5" />
+          <div className="flex flex-col gap-1 font-light text-sm">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border border-gray-200 px-5 py-3 text-sm cursor-pointer">
+            Description
+          </b>
+          <p className="border border-gray-200 px-5 py-3 text-sm cursor-pointer">
+            Reviews (122)
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 border border-gray-200 px-6 py-6 text-sm text-gray-500">
+          <p>
+            An e-commerce website is an online platform that facilitates the
+            buying and selling of products or services over the internet. It
+            serves as a virtual marketplace where businesses and individuals can
+            showcase their products, interact with customers, and conduct
+            transactions without the need for a physical presence. E-commerce
+            websites have gained immense popularity due to their convenience,
+            accessibility, and the global reach they offer.
+          </p>
+          <p>
+            E-commerce websites typically display products or services along
+            with detailed descriptions, images, prices, and any available
+            variations (e.g., sizes, colors). Each product usually has its own
+            dedicated page with relevant information.
+          </p>
         </div>
       </div>
     </div>

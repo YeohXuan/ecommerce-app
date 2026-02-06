448da4280bc6b19eab92e4a6baa327a9a1731be6
Yeoh Xuan <yorkxuan7@gmail.com>
2026-02-06 22:04:03 +0800

    Fixed hydration issue

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -30,37 +30,32 @@ const CartTotal = () => {
   };
 
   return (
-    <div className="flex justify-end my-20">
+    <div className="flex justify-end">
       <div className="w-full sm:w-112.5">
         <div className="text-2xl">
           <Title firstText={"CART"} secondText={"TOTALS"} />
           <div className="flex flex-col gap-2 mt-5 text-sm">
-            <p className="flex justify-between">
-              Subtotal
+            <div className="flex justify-between">
+              <p>Subtotal</p>
               <p>
                 {currency} {totalAmount}.00
               </p>
-            </p>
+            </div>
             <hr className="border-gray-300" />
-            <p className="flex justify-between">
-              Shipping Fee
+            <div className="flex justify-between">
+              <p>Shipping Fee</p>
               <p>
                 {currency} {shippingFee}.00
               </p>
-            </p>
+            </div>
             <hr className="border-gray-300" />
-            <p className="flex justify-between font-bold">
-              Total
+            <div className="flex justify-between font-bold">
+              <p>Total</p>
               <p>
                 {currency} {totalAmount > 0 ? totalAmount + shippingFee : 0}.00
               </p>
-            </p>
+            </div>
           </div>
-        </div>
-        <div className="w-full text-end">
-          <button className="bg-black text-[#f2f8fc] px-6 py-3 my-8 cursor-pointer text-sm">
-            PROCEED TO CHECKOUT
-          </button>
         </div>
       </div>
     </div>

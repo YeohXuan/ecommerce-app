853679aabbb2e8f2c4b4098f25cf6b77481ae4da
Yeoh Xuan <yorkxuan7@gmail.com>
2026-02-07 23:04:12 +0800

    Update Orders.jsx

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,17 @@
-const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+import { useContext } from "react";
+import Title from "../components/Title";
+import { ShopContext } from "../context/ShopContext";
 
-export default Orders
+const Orders = () => {
+  const { products, currency, cartItems } = useContext(ShopContext);
+
+  return (
+    <div className="border-t border-gray-300 pt-8">
+      <div className="text-2xl">
+        <Title firstText={"MY"} secondText={"ORDERS"} />
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
